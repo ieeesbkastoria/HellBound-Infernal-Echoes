0113a2c0a36d798025517f674f0e8406db5be6d4
Svarnik0 <104762662+Svarnik0@users.noreply.github.com>
2024-02-11 23:29:51 +0200

    Added Animations and Merged Scenes
    Added Run, Idle and Jump Animations.
    Added the Animation Controller for the Player Animations.
    Added Quit and Start Scripts for the Main UI.

--- Assets/Scripts/Level_Scripts/MamalakisMovement.cs
+++ Assets/Scripts/Level_Scripts/MamalakisMovement.cs
@@ -1,14 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events; // Add this line to include the UnityEngine.Events namespace
 
 public class MamalakisMovement : MonoBehaviour
 {
-   
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
     private float dirX;
     public int pz;
+
+    public Animator animator;
+
+    // Define events
+    [Header("Events")]
+    [Space]
+    public UnityEvent onLandingEvent;
+
+    private void Awake()
+    {
+        // Ensure event is initialized
+        if (onLandingEvent == null)
+            onLandingEvent = new UnityEvent();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,43 +35,51 @@ public class MamalakisMovement : MonoBehaviour
     void Update()
     {
         dirX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
 
+        animator.SetFloat("Speed", Mathf.Abs(dirX));
+        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, 10f);
+            animator.SetBool("IsJumping", true);
         }
 
         Flip123();
         pz = Posi();
     }
 
+
     private void Flip123()
     {
-        if (dirX > 0f)
+        if (dirX < 0f)
         {
             sprite.flipX = true;
         }
 
-        else if (dirX < 0f)
+        else if (dirX > 0f)
         {
             sprite.flipX = false;
         }
     } 
+
+    public void OnLanding()
+    {
+        Debug.Log("OnLanding function called.");
+        animator.SetBool("IsJumping", false);
+        onLandingEvent.Invoke();
+    }
     
     private int Posi()
     {
         if (dirX > 0f)
         {
-           return 1;
+            return 1;
         }
-
         else if (dirX < 0f)
         {
             return 2;
         }
-        
         else 
         {
             return 3;
